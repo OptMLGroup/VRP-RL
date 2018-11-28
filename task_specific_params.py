8766cced083dde9734d4527909e545d4f3d63d6f
Reza <reza.nazari@lehigh.edu>
2018-11-28 18:46:32 -0500

    fixed config for vrp10

--- task_specific_params.py
+++ task_specific_params.py
@@ -50,7 +50,7 @@ vrp10 = TaskVRP(task_name = 'vrp',
 			  input_dim=3,
 			  n_nodes=11,
 			  n_cust = 10,
-			  decode_len=15,
+			  decode_len=16,
 			  capacity=20,
 			  demand_max=9)
 task_lst['vrp10'] = vrp10
